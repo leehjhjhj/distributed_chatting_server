347a349a9fe243ef11bf4f16edc3843fb1f79a56
leehjhjhj <tsukiakarii@naver.com>
2023-11-21 01:04:49 +0900

    feat: 로그인 시리얼라이저 추가

--- members/serializers/member_serializer.py
+++ members/serializers/member_serializer.py
@@ -1,12 +1,32 @@
 from rest_framework import serializers
 from members.domains import Member
 from django.contrib.auth.hashers import make_password
+from utils.validators import validate_password, validate_email
 
 class SignupRequestSerializer(serializers.ModelSerializer):
-    
+    email = serializers.EmailField(validators=[validate_email])
+    password = serializers.CharField(validators=[validate_password])
+
     class Meta:
         model = Member
         fields = ('email', 'password', 'nickname')
 
     def validate_password(self, value):
         return make_password(value)
+    
+class SigninRequestSerialzier(serializers.ModelSerializer):
+    email = serializers.EmailField(validators=[])
+
+    class Meta:
+        model = Member
+        fields = ('email', 'password')
+
+class MemberResponseSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Member
+        fields = ('email', 'nickname')
+
+class SigninResponseSerializer(serializers.Serializer):
+    accessToken = serializers.CharField(source='access_token')
+    member = MemberResponseSerializer()
