4c9b6bb435a65b47f546dbcea3bf88fcf910aca7
leehjhjhj <tsukiakarii@naver.com>
2023-11-20 22:52:57 +0900

    chore: url, 컨테이너, jwt 설정

--- members/containers.py
+++ members/containers.py
@@ -1,4 +1,10 @@
 from dependency_injector import containers, providers
+from members.applications import MemberService
+from members.domains import MemberRepository
 
 class MembersContainer(containers.DeclarativeContainer):
-    pass
+    member_repository = providers.Factory(MemberRepository)
+    member_service = providers.Factory(
+        MemberService,
+        member_repository=member_repository
+    )
