d2aad2ed52d0949a62dd1c4a9922cb6520549282
leehjhjhj <tsukiakarii@naver.com>
2023-11-21 01:04:04 +0900

    feat: 멤버 레포 추가

--- members/domains/member_repository.py
+++ members/domains/member_repository.py
@@ -1,3 +1,6 @@
+from django.shortcuts import get_object_or_404
+from members.domains import Member
 
 class MemberRepository:
-    pass
+    def find_member_by_email(self, email: str):
+        return get_object_or_404(Member, email=email)
