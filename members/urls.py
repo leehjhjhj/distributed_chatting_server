b9d7149ea370595c83eaa1353460ca41f8717c5b
leehjhjhj <tsukiakarii@naver.com>
2023-11-21 01:05:08 +0900

    chore: 로그인 url 추가

--- members/urls.py
+++ members/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from members.presentations import SignUpView
+from members.presentations import SignUpView, SigninView
 
 app_name = 'members'
 
 urlpatterns = [
-    path('signup/', SignUpView.as_view())
+    path('signup/', SignUpView.as_view()),
+    path('signin/', SigninView.as_view()),
 ]
