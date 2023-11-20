fa82200de9ecd876b1868fbfb4fa6ee855c677a9
leehjhjhj <tsukiakarii@naver.com>
2023-11-21 01:04:28 +0900

    feat: 로그인로직 서비스 추가

--- members/applications/member_service.py
+++ members/applications/member_service.py
@@ -1,5 +1,13 @@
 from members.domains import MemberRepository
-from members.serializers import SignupRequestSerializer
+from members.serializers import (
+                            SignupRequestSerializer,
+                            SigninRequestSerialzier,
+                            SigninResponseSerializer,
+                        )
+from members.domains import Member
+from django.contrib.auth.hashers import check_password
+from rest_framework_simplejwt.tokens import RefreshToken, Token
+from utils.exceptions import PasswordWrongError
 
 class MemberService:
     def __init__(self, member_repository: MemberRepository, *args, **kwargs):
@@ -9,4 +17,20 @@ class MemberService:
         signup_request_serializer = SignupRequestSerializer(data=request_data)
         signup_request_serializer.is_valid(raise_exception=True)
         signup_request_serializer.save()
-        
+    
+    def signin(self, request_data: dict):
+        signin_request_serializer = SigninRequestSerialzier(data=request_data)
+        signin_request_serializer.is_valid(raise_exception=True)
+        signin_data: dict = signin_request_serializer.validated_data
+
+        email = signin_data.get('email')
+        password = signin_data.get('password')
+        member: Member = self._member_repository.find_member_by_email(email=email)
+
+        if not check_password(password, member.password):
+            raise PasswordWrongError
+
+        refresh: RefreshToken = RefreshToken.for_user(member)
+        access_token = refresh.access_token
+        signin_serializer = SigninResponseSerializer({'access_token': access_token, 'member': member})
+        return signin_serializer.data
