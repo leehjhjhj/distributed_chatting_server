442bd20880d774d77f6d77564435207ac63474b6
leehjhjhj <tsukiakarii@naver.com>
2023-11-22 18:11:37 +0900

    chore: settings.py channel layer 추가

--- server/settings/base.py
+++ server/settings/base.py
@@ -125,6 +125,14 @@ USE_L10N = True
 
 USE_TZ = True
 
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [('redis', 6379)],
+        },
+    },
+}
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
