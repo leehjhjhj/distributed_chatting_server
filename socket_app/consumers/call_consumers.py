7704604fbd0b1cca2304b305dcd23fb4492306e9
leehjhjhj <tsukiakarii@naver.com>
2023-11-24 15:13:40 +0900

    feat: 컨슈머에 채팅에 닉네임과 날짜 추가

--- socket_app/consumers/call_consumers.py
+++ socket_app/consumers/call_consumers.py
@@ -2,12 +2,19 @@ from channels.generic.websocket import JsonWebsocketConsumer
 from asgiref.sync import async_to_sync
 from chats.domains import Chat
 import logging
+from datetime import datetime
+
+def current_time():
+    now = datetime.now()
+    formatted_now = now.strftime('%Y-%m-%d %H:%M:%S')
+    return formatted_now
 
 class ChatConsumer(JsonWebsocketConsumer):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.group_name = ""
-    
+        self.nickname = ""
+
     def receive_json(self, content, **kwargs):
         _type = content["type"]
         _message = content["message"]
@@ -27,6 +34,9 @@ class ChatConsumer(JsonWebsocketConsumer):
         chat_id = self.scope["url_route"]["kwargs"]["chat_id"]
         self.group_name = Chat.make_chat_group_name(chat_id)
 
+        user = self.scope["user"]
+        self.nickname = user.nickname
+
         async_to_sync(self.channel_layer.group_add)(
             self.group_name,
             self.channel_name,
@@ -43,5 +53,7 @@ class ChatConsumer(JsonWebsocketConsumer):
     def chat_message(self, message_dict):
         self.send_json({
             "type": "chat.message",
-            "message": message_dict["message"]
+            "message": message_dict["message"],
+            "nickname": self.nickname,
+            "time": current_time()
         })
