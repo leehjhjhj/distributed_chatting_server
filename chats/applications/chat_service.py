c8470d55e7931108b493d2984af6c902d0d28905
leehjhjhj <tsukiakarii@naver.com>
2023-11-25 13:13:59 +0900

    faet: 채팅 더보기, 입장 관련 서비스 작성

--- chats/applications/chat_service.py
+++ chats/applications/chat_service.py
@@ -1,6 +1,8 @@
 from chats.domains import ChatRepository, Participant, Chat
-from chats.serializers import ChatCreateRequestSerializer
+from chats.serializers import ChatCreateRequestSerializer, ChatMessageResponseSerializer, ChatMessageSerializer
 from members.domains import Member
+from boto3.dynamodb.conditions import Key
+from utils.connect_dynamodb import get_dynamodb_table
 
 class ChatService:
     def __init__(self, chat_repository: ChatRepository, *args, **kwargs):
@@ -18,3 +20,40 @@ class ChatService:
                 max_capacity=chat_data.get('max_capacity')
                 )
         self._chat_repository.save_chat(new_chat)
+
+    def join_chat(self, user_data: Member, chat_id: str):
+        table = get_dynamodb_table()
+        query_params = {
+            'KeyConditionExpression': Key('chat_id').eq(chat_id),
+            'ScanIndexForward': False,
+            'Limit': 20
+        }
+        response = table.query(**query_params)
+        old_messages = response.get('Items')
+        chat_message_response_serialzier = ChatMessageSerializer(old_messages, many=True)
+        return chat_message_response_serialzier.data
+    
+    def get_more_messages(self, chat_id: str, last_evaluated_key: str):
+        table = get_dynamodb_table()
+        query_params = {
+            'KeyConditionExpression': Key('chat_id').eq(chat_id),
+            'ScanIndexForward': False,
+            'Limit': 10
+        }
+        if last_evaluated_key is not None:
+            exclusive_start_key = {
+                "chat_id": chat_id,
+                "timestamp": last_evaluated_key
+            }
+            query_params['ExclusiveStartKey'] = exclusive_start_key
+            
+        response = table.query(**query_params)
+        old_messages = response.get('Items')
+        last_evaluated_key = response.get('LastEvaluatedKey', None)
+        chat_message_response_serialzier = ChatMessageResponseSerializer(self._ChatResponseDto(old_messages, last_evaluated_key))
+        return chat_message_response_serialzier.data
+    
+    class _ChatResponseDto:
+        def __init__(self, old_messages: list[dict], last_evaluated_key: dict):
+            self.old_messages = old_messages
+            self.last_evaluated_key = last_evaluated_key
