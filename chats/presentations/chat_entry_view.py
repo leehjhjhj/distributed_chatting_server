e85fdf987cac0cd923be179bc8900ef1e1fa55e2
leehjhjhj <tsukiakarii@naver.com>
2023-11-25 13:13:13 +0900

    feat: 채팅 입장 view 작성

---
+++ chats/presentations/chat_entry_view.py
@@ -0,0 +1,16 @@
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from chats.containers import ChatsContainer
+
+class ChatEntryView(APIView):
+    authentication_classes = [JWTAuthentication]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._chat_service = ChatsContainer.chat_service()
+
+    def post(self, request, chat_id: int):
+        response = self._chat_service.join_chat(request.user, chat_id)
+        return Response(response, status=status.HTTP_200_OK)
