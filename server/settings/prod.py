9c4fde5d27fdfd182bc90d4eccf9bd6aa19a60e3
leehjhjhj <tsukiakarii@naver.com>
2023-11-26 19:05:38 +0900

    refactor: 레디스 host import 방식 변경

--- server/settings/prod.py
+++ server/settings/prod.py
@@ -20,7 +20,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [('redis', 6379)],
+            "hosts": [(config('REDIS_HOST'), 6379)],
         },
     },
 }
