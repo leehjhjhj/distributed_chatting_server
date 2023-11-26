d5187c3c647795b4898b64e2b106fd63e458cdb5
leehjhjhj <tsukiakarii@naver.com>
2023-11-26 16:06:52 +0900

    HOTFIX: asgi 모듈 호출 방식 변경

--- server/asgi.py
+++ server/asgi.py
@@ -2,7 +2,6 @@
 import os
 from channels.routing import URLRouter, ProtocolTypeRouter
 from django.core.asgi import get_asgi_application
-from socket_app.routings import chat_routing
 from django_channels_jwt_auth_middleware.auth import JWTAuthMiddlewareStack
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'server.local.settings')
@@ -13,7 +12,7 @@ application = ProtocolTypeRouter({
     "http": django_asgi_app,
     "websocket": JWTAuthMiddlewareStack(
         URLRouter(
-            chat_routing.websocket_urlpatterns
+            'socket_app.routings.chat_routing.websocket_urlpatterns'
         )
     )
 })
