c3fb85df15d00b611ec020ee6ae318f803c5e033
leehjhjhj <tsukiakarii@naver.com>
2023-11-26 19:47:58 +0900

    add: decode_responses 추가

--- utils/redis_utils.py
+++ utils/redis_utils.py
@@ -4,7 +4,7 @@ import logging
 
 def get_redis_connection(db_select: int):
     REDIS_HOST=config('REDIS_HOST')
-    redis_con = Redis(host=REDIS_HOST, port=6379, db=db_select)
+    redis_con = Redis(host=REDIS_HOST, port=6379, db=db_select, decode_responses=True)
     try:
         if not redis_con.ping():
             return None
