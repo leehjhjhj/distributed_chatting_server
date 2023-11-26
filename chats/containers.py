926a553d76179a6f4946bebfe3f7f5fe8f017a0c
leehjhjhj <tsukiakarii@naver.com>
2023-11-26 18:08:14 +0900

    chore: 챗 조회 관련 Url, container 설정

--- chats/containers.py
+++ chats/containers.py
@@ -1,10 +1,14 @@
 from dependency_injector import containers, providers
 from chats.domains import ChatRepository
-from chats.applications import ChatService
+from chats.applications import ChatService, ChatGetService
 
 class ChatsContainer(containers.DeclarativeContainer):
     chat_repository=providers.Factory(ChatRepository)
     chat_service=providers.Factory(
         ChatService,
         chat_repository=chat_repository
+    )
+    chat_get_service = providers.Factory(
+        ChatGetService,
+        chat_repository
     )
--- chats/urls.py
+++ chats/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from chats.presentations import ChatView, ChatEntryView, ChatGetMoreMessageView
+from chats.presentations import ChatView, ChatEntryView, ChatGetMoreMessageView, ChatRoomGetView, ChatDetailView
 
 app_name = 'chats'
 
 urlpatterns = [
     path('create/', ChatView.as_view()),
     path('<str:chat_id>/join/', ChatEntryView.as_view()),
-    path('<str:chat_id>/more/', ChatGetMoreMessageView.as_view())
+    path('<str:chat_id>/more/', ChatGetMoreMessageView.as_view()),
+    path('', ChatRoomGetView.as_view()),
+    path('<int:chat_id>/', ChatDetailView.as_view())
 ]
