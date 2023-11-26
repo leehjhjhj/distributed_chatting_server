6035deecb7d1706db7d248088c4c10a7141ca934
leehjhjhj <tsukiakarii@naver.com>
2023-11-26 20:25:10 +0900

    feat: 채팅룸 조회시 members도 패치조인

--- chats/domains/chat_repository.py
+++ chats/domains/chat_repository.py
@@ -5,8 +5,8 @@ class ChatRepository:
     def save_chat(self, chat: Chat):
         chat.save()
     
-    def find_all_chat_rooms(self):
-        return get_list_or_404(Chat)
+    def find_all_chat_rooms_with_members(self):
+        return get_list_or_404(Chat.objects.select_related('made_by'))
     
     def find_chat_by_id(self, chat_id: int):
         return get_object_or_404(Chat, id=chat_id)
