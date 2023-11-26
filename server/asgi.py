923eb093679a020d86ea14f6233a9c232cb24202
leehjhjhj <tsukiakarii@naver.com>
2023-11-26 16:33:40 +0900

    HOTFIX: asgi import 순서 변경

--- server/asgi.py
+++ server/asgi.py
@@ -1,13 +1,14 @@
 
 import os
-from channels.routing import URLRouter, ProtocolTypeRouter
 from django.core.asgi import get_asgi_application
-from socket_app.routings import chat_routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'server.local.settings')
-
 django_asgi_app = get_asgi_application()
+
+from channels.routing import URLRouter, ProtocolTypeRouter
+from socket_app.routings import chat_routing
 from django_channels_jwt_auth_middleware.auth import JWTAuthMiddlewareStack
+
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
     "websocket": JWTAuthMiddlewareStack(
