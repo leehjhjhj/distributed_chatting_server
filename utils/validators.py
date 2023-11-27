8ff7fe58ded775687d27f0048b249faaec060620
leehjhjhj <tsukiakarii@naver.com>
2023-11-27 16:35:58 +0900

    refactor: nickname validator추가 및 멘트 변경

--- utils/validators.py
+++ utils/validators.py
@@ -1,17 +1,22 @@
 import re
 from django.utils.translation import gettext_lazy as _
 from django.core.exceptions import ValidationError
+from members.domains import Member
 
 def validate_password(password):
     password_reg = r"^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{6,13}$"
     password_regex = re.compile(password_reg)
 
     if not password_regex.match(password):
-        raise ValidationError("영문, 숫자, 특수문자 조합해 6자 이상, 13자 이하 입력해주세요.")
+        raise ValidationError("영문, 숫자, 특수문자를 조합해 6자 이상, 13자 이하 입력해주세요.")
     
 def validate_email(email):
     email_reg = r"^[a-zA-Z0-9_-]{6,13}@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
     email_regex = re.compile(email_reg)
 
     if not email_regex.match(email):
-        raise ValidationError("이메일 형식이 잘못되었습니다. 아이디는 6자 이상 13자 이하로 작성하고, 특수 문자는 _와 -만 사용 가능합니다.")
+        raise ValidationError("아이디는 6자 이상 13자 이하로 가능하고, 특수 문자는 _와 -만 사용 가능해요.")
+    
+def validate_nickname(nickname):
+    if Member.objects.get(nickname=nickname):
+        raise ValidationError("닉네임이 이미 존재해요! 다른 걸로 부탁해요.")
