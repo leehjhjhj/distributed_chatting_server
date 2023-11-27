5ead83d897363cffb22772cffebb9c39afdec4bf
leehjhjhj <tsukiakarii@naver.com>
2023-11-27 11:07:05 +0900

    feat: 접속 유저 조회 뷰 생성

--- chats/presentations/__init__.py
+++ chats/presentations/__init__.py
@@ -3,3 +3,4 @@ from .chat_entry_view import ChatEntryView
 from .chat_more_messages_view import ChatGetMoreMessageView
 from .chat_room_get_view import ChatRoomGetView
 from .chat_detail_view import ChatDetailView
+from .chat_joined_members_view import ChatJoinedMembersView
