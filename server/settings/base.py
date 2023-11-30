5860124ff456729f7574afb9562a6639d580b93b
leehjhjhj <tsukiakarii@naver.com>
2023-11-30 20:56:39 +0900

    add: CORS 주소 추가

--- server/settings/base.py
+++ server/settings/base.py
@@ -18,6 +18,8 @@ SECRET_KEY = config('SECRET_KEY')
 ALLOWED_HOSTS = ['*']
 CORS_ALLOWED_ORIGINS = [
 	'http://localhost:3000',
+    'http://localhost:5173',
+    'https://localhost:5173',
 ]
 
 # Application definition
