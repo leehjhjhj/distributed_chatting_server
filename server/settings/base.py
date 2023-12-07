141ba7c4f38e9aab494b7d58c736b62bc44c86fb
leehjhjhj <tsukiakarii@naver.com>
2023-12-07 13:03:20 +0900

    refactor: settings.py blacklist 등록 삭제

--- server/settings/base.py
+++ server/settings/base.py
@@ -91,7 +91,7 @@ SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'ROTATE_REFRESH_TOKENS': False,
-    'BLACKLIST_AFTER_ROTATION': True,
+    'BLACKLIST_AFTER_ROTATION': False,
     'AUTH_HEADER_TYPES': ('Bearer',),
     'AUTH_HEADER_NAME': 'HTTP_AUTHORIZATION',
     'USER_ID_FIELD': 'id',
