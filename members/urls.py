2c2426e386b5782eb0553e6932ad9d0a78034831
leehjhjhj <tsukiakarii@naver.com>
2023-12-08 15:46:12 +0900

    feat: 로그아웃 url 추가

--- members/urls.py
+++ members/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from members.presentations import SignUpView, SigninView
+from members.presentations import SignUpView, SigninView, LogoutView
 
 app_name = 'members'
 
 urlpatterns = [
     path('signup/', SignUpView.as_view()),
     path('signin/', SigninView.as_view()),
+    path('logout/', LogoutView.as_view()),
 ]
