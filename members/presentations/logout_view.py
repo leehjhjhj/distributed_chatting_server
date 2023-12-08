261bb3cdce6070597761a691d6e2fd7095240ad4
leehjhjhj <tsukiakarii@naver.com>
2023-12-08 16:05:39 +0900

    fix: 로그아웃뷰 APIview로 변경

--- members/presentations/logout_view.py
+++ members/presentations/logout_view.py
@@ -1,10 +1,10 @@
-from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from members.containers import MembersContainer
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
-class LogoutView(TokenObtainPairView):
+class LogoutView(APIView):
     authentication_classes = [JWTAuthentication]
 
     def __init__(self, *args, **kwargs):
