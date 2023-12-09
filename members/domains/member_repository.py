9cf10e6bb7482f5af6057f3f9b8d36ae0b6e8c3b
leehjhjhj <tsukiakarii@naver.com>
2023-12-09 12:35:03 +0900

    feat: 토큰 리프레시 관련 레포 로직 추가

--- members/domains/member_repository.py
+++ members/domains/member_repository.py
@@ -4,3 +4,6 @@ from members.domains import Member
 class MemberRepository:
     def find_member_by_email(self, email: str):
         return get_object_or_404(Member, email=email)
+    
+    def find_member_by_id(self, member_id: int):
+        return get_object_or_404(Member, id=member_id)
