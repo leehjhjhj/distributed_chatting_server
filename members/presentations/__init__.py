93ae3915e99577c5b31d041139bba3ed13474e7d
leehjhjhj <tsukiakarii@naver.com>
2023-12-09 12:33:31 +0900

    refactor: 로그아웃시 엑세스 토큰 회원 인증 제거

--- members/presentations/__init__.py
+++ members/presentations/__init__.py
@@ -1,3 +1,4 @@
 from .signup_view import SignUpView
 from .signin_view import SigninView
 from .logout_view import LogoutView
+from .refresh_access_token_view import RefreshAccessTokenView
--- members/presentations/logout_view.py
+++ members/presentations/logout_view.py
@@ -2,15 +2,13 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from members.containers import MembersContainer
-from rest_framework_simplejwt.authentication import JWTAuthentication
 
 class LogoutView(APIView):
-    authentication_classes = [JWTAuthentication]
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self._members_service = MembersContainer.member_service()
 
     def post(self, request, *args, **kwargs):
-        response = self._members_service.logout(request.data, request.user)
+        response = self._members_service.logout(request.data)
         return Response(response, status=status.HTTP_204_NO_CONTENT)
