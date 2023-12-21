46268943bd6a0f1e19a16961daa23d1c56e5c2c9
leehjhjhj <tsukiakarii@naver.com>
2023-12-22 07:41:11 +0900

    refactor: 채팅방 삭제 예외 이름 변경

--- utils/exceptions/chat_exception.py
+++ utils/exceptions/chat_exception.py
@@ -5,6 +5,6 @@ class OverMaxCountError(APIException):
     status_code = status.HTTP_400_BAD_REQUEST
     default_detail = '채팅방이 꽉 찼습니다.'
 
-class NoRightToDeleteChat(APIException):
+class NoRightToDeleteChatError(APIException):
     status_code = status.HTTP_403_FORBIDDEN
     default_detail = '채팅창을 삭제할 권한이 없습니다.'
