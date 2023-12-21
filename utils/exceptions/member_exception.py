e83f80a9a22eb54c6a1dc41bab28396e27daaac8
leehjhjhj <tsukiakarii@naver.com>
2023-12-22 08:28:56 +0900

    feat: 로그인 필요 예외 작성

--- utils/exceptions/member_exception.py
+++ utils/exceptions/member_exception.py
@@ -1,7 +1,10 @@
-from rest_framework.exceptions import AuthenticationFailed, APIException
+from rest_framework.exceptions import APIException
 from rest_framework import status
-from django.core.exceptions import ValidationError
 
 class PasswordWrongError(APIException):
     status_code = status.HTTP_401_UNAUTHORIZED
     default_detail = '비밀번호가 틀렸습니다. 다시 확인해주세요.'
+
+class RequiredLoginError(APIException):
+    status_code = status.HTTP_401_UNAUTHORIZED
+    default_detail = '로그인이 필요합니다.'
