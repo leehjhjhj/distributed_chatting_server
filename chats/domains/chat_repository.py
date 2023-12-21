c0fd36689e99778e8ff78a413e676016882e63dc
leehjhjhj <tsukiakarii@naver.com>
2023-12-21 23:03:04 +0900

    refactor: 채팅방 전체 조회 정렬 추가

--- chats/domains/chat_repository.py
+++ chats/domains/chat_repository.py
@@ -6,7 +6,7 @@ class ChatRepository:
         chat.save()
     
     def find_all_chat_rooms_with_members(self):
-        return get_list_or_404(Chat.objects.select_related('made_by'))
+        return get_list_or_404(Chat.objects.select_related('made_by').order_by('-created_at'))
     
     def find_chat_by_id(self, chat_id: int):
         return get_object_or_404(Chat, id=chat_id)
