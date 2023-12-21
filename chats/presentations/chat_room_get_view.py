dc51313f9de4501f76133b8f5c923ab89bb5c1e9
leehjhjhj <tsukiakarii@naver.com>
2023-12-22 08:29:32 +0900

    refactor: 쿼리파라미터 추가 및 유저 데이터 추가

--- chats/presentations/chat_room_get_view.py
+++ chats/presentations/chat_room_get_view.py
@@ -2,13 +2,16 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
 from chats.containers import ChatsContainer
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
 class ChatRoomGetView(APIView):
+    authentication_classes = [JWTAuthentication]
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self._chat_get_service = ChatsContainer.chat_get_service()
 
-    def get(self, request):
-        response = self._chat_get_service.get_all_chat_rooms()
+    def get(self, request, *args, **kwargs):
+        order_by = request.query_params.get('order', 'latest')
+        response = self._chat_get_service.get_all_chat_rooms(order_by, request.user)
         return Response(response, status=status.HTTP_200_OK)
