13d5451063a9df52a0f746593c5da98477bdc0c5
leehjhjhj <tsukiakarii@naver.com>
2023-12-24 16:13:12 +0900

    chore: 시작시 search lookup 로드

--- server/settings/base.py
+++ server/settings/base.py
@@ -2,6 +2,7 @@ from pathlib import Path
 from decouple import config
 import os
 from datetime import timedelta
+from utils.lookups import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
