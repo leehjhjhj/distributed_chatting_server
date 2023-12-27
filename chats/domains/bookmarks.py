7802f20817dd0f8f104c5ae2c415124c7b0aa7c0
leehjhjhj <tsukiakarii@naver.com>
2023-12-27 15:13:08 +0900

    feat: bookmarks에 양방향 매핑 추가

--- chats/domains/bookmarks.py
+++ chats/domains/bookmarks.py
@@ -2,8 +2,8 @@ from django.db import models
 
 class Bookmark(models.Model):
     id = models.BigAutoField(primary_key=True)
-    member = models.ForeignKey('members.Member', models.DO_NOTHING)
-    chat = models.ForeignKey('Chat', models.CASCADE)
+    member = models.ForeignKey('members.Member', models.DO_NOTHING, related_name='bookmarks')
+    chat = models.ForeignKey('Chat', models.CASCADE, related_name='bookmarks')
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
