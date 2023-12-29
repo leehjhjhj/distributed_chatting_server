15ca9ffbe8a5625dc0e59c479d4e09b28b85dab1
leehjhjhj <tsukiakarii@naver.com>
2023-12-29 17:32:55 +0900

    feat: 북마크 레포 생성

--- chats/domains/__init__.py
+++ chats/domains/__init__.py
@@ -2,3 +2,4 @@ from .chats import Chat
 from .participants import Participant
 from .bookmarks import Bookmark
 from .chat_repository import ChatRepository
+from .bookmark_repository import BookmarkRepository
