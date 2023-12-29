453873170f85c3ecb959f2b4de60e6e39c09fbec
leehjhjhj <tsukiakarii@naver.com>
2023-12-29 17:18:19 +0900

    feat: 북마크 생성 view 작성

---
+++ chats/presentations/bookmark_create_view.py
@@ -0,0 +1,18 @@
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from chats.containers import ChatsContainer
+from rest_framework.permissions import IsAuthenticated
+
+class BookmarkCreateView(APIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._bookmark_service = ChatsContainer.bookmark_service()
+
+    def post(self, request):
+        self._bookmark_service.create_bookmark(request.data, request.user)
+        return Response(status=status.HTTP_201_CREATED)
