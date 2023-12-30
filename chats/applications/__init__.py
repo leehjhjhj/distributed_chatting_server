ec0bc5124d6cd98d655585b5765650effdb4aa37
leehjhjhj <tsukiakarii@naver.com>
2023-12-30 19:33:40 +0900

    feat: 즐겨찾기 등록 기능 서비스 로직 추가

--- chats/applications/__init__.py
+++ chats/applications/__init__.py
@@ -1,2 +1,3 @@
 from .chat_service import ChatService
 from .chat_get_service import ChatGetService
+from .bookmark_service import BookmarkService
---
+++ chats/applications/bookmark_service.py
@@ -0,0 +1,18 @@
+from chats.domains import ChatRepository, BookmarkRepository, Chat, Bookmark
+from chats.serializers import BookmarkCreateRequestSerializer
+
+class BookmarkService:
+    def __init__(self, bookmark_repository: BookmarkRepository, chat_repository: ChatRepository, *args, **kwargs):
+        self._bookmark_repository = bookmark_repository
+        self._chat_repository = chat_repository
+
+    def create_bookmark(self, request_data: dict, user_data: dict):
+        bookmark_create_request_serialzier = BookmarkCreateRequestSerializer(data=request_data)
+        bookmark_create_request_serialzier.is_valid(raise_exception=True)
+        bookmark_data = bookmark_create_request_serialzier.validated_data
+
+        chat_id = bookmark_data.get('chat_id')
+        chat: Chat = self._chat_repository.find_chat_by_id(chat_id)
+
+        bookmark = Bookmark(member=user_data, chat=chat)
+        self._bookmark_repository.save_bookmark(bookmark)
