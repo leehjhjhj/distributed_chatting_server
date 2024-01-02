1996df570b42d81e7957c1db6b96769685e43533
leehjhjhj <tsukiakarii@naver.com>
2024-01-02 11:12:40 +0900

    feat: member, chat에 인덱스 달기

--- chats/domains/bookmarks.py
+++ chats/domains/bookmarks.py
@@ -2,9 +2,12 @@ from django.db import models
 
 class Bookmark(models.Model):
     id = models.BigAutoField(primary_key=True)
-    member = models.ForeignKey('members.Member', models.DO_NOTHING, related_name='bookmarks')
+    member = models.ForeignKey('members.Member', models.CASCADE, related_name='bookmarks')
     chat = models.ForeignKey('Chat', models.CASCADE, related_name='bookmarks')
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         db_table = 'bookmarks'
+        indexes = [
+            models.Index(fields=['member', 'chat']),
+        ]
